07ca321a1051f2a060f7cd14e4d517879fbf7c4b
Immolazione <federico.deered@gmail.com>
2022-01-14 18:00:52 +0100

    reset

--- js/script1.js
+++ js/script1.js
@@ -5,14 +5,3 @@ console.log('JS OK');
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // CHIEDO ALL'UTENTE DI SCRIVERE UNA PAROLA
-const word = prompt('Inserisci una parola').trim();
-
-// INVOCAZIONE
-reversedWord(word);
-
-function reversedWord(word) {
-    // REVERSE DELLA PAROLA
-    
-
-    // CONFRONTO PAROLA E PAROLA REVERSE
-}
