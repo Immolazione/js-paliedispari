af491253fbd6b27922eab4099e21b36092b3b1aa
Immolazione <federico.deered@gmail.com>
2022-01-14 16:03:02 +0100

    attempt function palindrome

--- js/script1.js
+++ js/script1.js
@@ -3,3 +3,13 @@ console.log('JS OK');
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+// CHIEDO ALL'UTENTE DI SCRIVERE UNA PAROLA
+const word = prompt('Inserisci una parola').trim();
+
+function reversedWord(word) {
+    // REVERSE DELLA PAROLA
+    
+
+    // CONFRONTO PAROLA E PAROLA REVERSE
+}
