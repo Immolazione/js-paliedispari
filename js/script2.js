9256b7e1c68832a8e562f1a035a7861f2f470db2
Immolazione <federico.deered@gmail.com>
2022-01-16 17:29:31 +0100

    struggle and despair to solve the assignment

--- js/script2.js
+++ js/script2.js
@@ -12,3 +12,42 @@ const choiceField = document.getElementById('choice');
 const numberField = document.getElementById('number');
 const playButton = document.getElementById('play');
 const winnerField = document.getElementById('winner');
+
+let userNumber = numberField.value;
+let userChoice = choiceField.value;
+
+console.log(userNumber);
+console.log(userChoice);
+
+let result;
+
+playButton.addEventListener('click', function () {
+
+
+
+
+    // RICHIAMO NUMERO PC
+    const pcNumber = rndNumber(1, 5);
+
+    // FUNCTION PER NUMERO PC
+    function rndNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    console.log(rndNumber);
+    
+    // SOMMA
+    let sum = userNumber + pcNumber;
+    console.log(sum);
+    
+    // FUNCTION PER PARI O DISPARI
+    function oddEven(sum) {
+        if (sum % 2 == 0)
+          return "pari";
+        else
+          return "dispari";
+    }
+    result = oddEven(sum);
+    
+    // STAMPO IN PAGINA VINCITORE
+    winnerField.innerHTML = `La somma è: ${sum}`;
+})
