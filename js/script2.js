4a07fe33de3496995a966ae4d1054b424f192c4e
Immolazione <federico.deered@gmail.com>
2022-01-16 22:43:41 +0100

    try again

--- js/script2.js
+++ js/script2.js
@@ -7,47 +7,90 @@ console.log('JS OK');
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // RECUPERO ELEMENTI
-const choiceField = document.getElementById('choice');
-const numberField = document.getElementById('number');
-const playButton = document.getElementById('play');
-const winnerField = document.getElementById('winner');
+// const choiceField = document.getElementById('choice');
+// const numberField = document.getElementById('number');
+// const playButton = document.getElementById('play');
+// const winnerField = document.getElementById('winner');
 
-let userNumber = numberField.value;
-let userChoice = choiceField.value;
+// let userNumber = numberField.value;
+// let userChoice = choiceField.value;
 
-console.log(userNumber);
-console.log(userChoice);
+// console.log(userNumber);
+// console.log(userChoice);
 
-let result;
+// let result;
 
-playButton.addEventListener('click', function () {
-
-
+// playButton.addEventListener('click', function () {
 
 
-    // RICHIAMO NUMERO PC
-    const pcNumber = rndNumber(1, 5);
 
-    // FUNCTION PER NUMERO PC
-    function rndNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    console.log(rndNumber);
+
+//     // RICHIAMO NUMERO PC
+//     const pcNumber = rndNumber(1, 5);
+
+//     // FUNCTION PER NUMERO PC
+//     function rndNumber(min, max) {
+//         return Math.floor(Math.random() * (max - min + 1)) + min;
+//     }
+//     console.log(rndNumber);
     
-    // SOMMA
-    let sum = userNumber + pcNumber;
-    console.log(sum);
+//     // SOMMA
+//     let sum = userNumber + pcNumber;
+//     console.log(sum);
     
-    // FUNCTION PER PARI O DISPARI
-    function oddEven(sum) {
-        if (sum % 2 == 0)
-          return "pari";
-        else
-          return "dispari";
-    }
-    result = oddEven(sum);
+//     // FUNCTION PER PARI O DISPARI
+//     function oddEven(sum) {
+//         if (sum % 2 == 0)
+//           return "pari";
+//         else
+//           return "dispari";
+//     }
+//     result = oddEven(sum);
     
-    // STAMPO IN PAGINA VINCITORE
-    winnerField.innerHTML = `La somma è: ${sum}`;
-})
+//     // STAMPO IN PAGINA VINCITORE
+//     winnerField.innerHTML = `La somma è: ${sum}`;
+// })
